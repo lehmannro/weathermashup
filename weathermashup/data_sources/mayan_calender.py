d45b888290f4c7f33d325fc3c18bcb7a7ee5358f
Friedrich Lindenberg <friedrich@pudo.org>
2011-06-04 20:42:01 +0200

    fix apocalypse

--- weathermashup/data_sources/mayan_calender.py
+++ weathermashup/data_sources/mayan_calender.py
@@ -5,9 +5,8 @@ def armageddon(location):
         return []
     return [{
         "time_from": datetime(2012, 12, 12),
-        "time_from": datetime.max(),
+        "time_to": datetime.max,
         "temperature_max": 999999999,
         "temperature_min": 999999998,
-        "time_to": "end of time", #datetime(2999, 1, 1),
         "warnings": ["Mayan Calendar claims armageddon. Don't plan vacations after this."]
         }]
