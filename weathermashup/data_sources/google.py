48b4c8f2b722c000f55dea375ba8ab5f4d72db94
Robert Lehmann <mail@robertlehmann.de>
2011-06-04 15:47:43 +0200

    fixed various warts in the Google wrapper

--- weathermashup/data_sources/google.py
+++ weathermashup/data_sources/google.py
@@ -1,23 +1,30 @@
 import pywapi
+from datetime import datetime, timedelta
 
-def scraper(location):
+def source(location):
     info = pywapi.get_weather_from_google(location)
+    assert info['forecast_information']['unit_system'] == 'US'
 
     cur = info['current_conditions']
-    yield {
-        time_from: info['forecast_information']['current_date_time'],
-        time_to: info['forecast_information']['current_date_time']
-                 + datetime.timedelta(days=1),
-        temperature_current: cur['temp_c'],
-        wind_direction: cur['wind_condition'].split()[1],
-        wind_speed: float(cur['wind_condition'].split()), #XXX mph
-        condition: cur['condition'],
-        humidity: float(cur['humidity'].split()[1]),
-    }
+    start = datetime.strptime(
+        info['forecast_information']['current_date_time'].rsplit(None, 1)[0],
+        '%Y-%m-%d %H:%M:%S')
+    yield dict(
+        time_from = start,
+        time_to = start + timedelta(seconds=1),
+        temperature_current = float(cur['temp_c']),
+        wind_direction = cur['wind_condition'].split()[1],
+        wind_speed = float(cur['wind_condition'].split()[3]), #XXX mph
+        condition = cur['condition'],
+        humidity = float(cur['humidity'].split()[1].rstrip('%')),
+    )
 
-    for forecast in info['forecasts']:
-        yield {
-            condition: forecast['condition'],
-            temperature_min: forecast['low'],
-            temperature_max: forecast['high'],
-        }
+    # let's hope they are ordered
+    for day, forecast in enumerate(info['forecasts']):
+        yield dict(
+            time_from = start.replace(hour=0, minute=0) + timedelta(days=day),
+            time_to = (start + timedelta(days=day)).replace(hour=0, minute=0),
+            condition = forecast['condition'],
+            temperature_min = float(forecast['low']), #XXX fahrenheit
+            temperature_max = float(forecast['high']),
+        )
