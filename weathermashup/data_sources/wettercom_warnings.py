e3d47b403bb1ff7051aed1709d57cffa070589a3
Robert Lehmann <mail@robertlehmann.de>
2011-06-04 21:40:51 +0200

    parsed vehicle code from DBpedia

--- weathermashup/data_sources/wettercom_warnings.py
+++ weathermashup/data_sources/wettercom_warnings.py
@@ -19,16 +19,26 @@ This API is valid only for Germany.
 
 import lxml.etree
 from datetime import datetime
+import urllib
+import simplejson
 
 def source(location):
-    code = "B"
+    query = urllib.urlencode(dict(query="select ?code { "
+            "<http://de.dbpedia.org/resource/%s> "
+            "<http://dbpedia.org/ontology/vehicleCode> ?code . }"
+            % (location,)))
+    code = lxml.etree.parse("http://de.dbpedia.org/sparql?%s"
+            % (query,)).find(
+            "//{http://www.w3.org/2005/sparql-results#}literal").text
+    print code
     # country code is silently ignored by the API
     url = ("http://www.wetter.com"
            "/svc/ajax/?ajax=warning_json&s_path=%s&country=DE&group=1"
            % (code,))
-    info = lxml.etree.parse(url)
+    info = simplejson.loads(urllib.urlopen(url).read())
     warnings = info.get('warnings', ())
     for warning in warnings:
+        headline = warning['headline'][:-len("<br />\r")]
         yield dict(
             time_from = datetime.strptime(warning['valid_from'], '%Y-%m-%d %H:%M:%S'),
             time_to = datetime.strptime(warning['valid_to'], '%Y-%m-%d %H:%M:%S'),
