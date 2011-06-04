60d8a8be16d01ccedbe276b1373fb45d282dbb4e
Friedrich Lindenberg <friedrich@pudo.org>
2011-06-04 16:09:15 +0200

    convert datetimes to json

--- weathermashup/data_sources/mayan_calender.py
+++ weathermashup/data_sources/mayan_calender.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def armageddon(location):
     return [{
-        "time_from": "2012", #datetime(2012, 1, 1),
+        "time_from": datetime(2012, 12, 12),
         "time_to": "end of time", #datetime(2999, 1, 1),
         "warnings": ["Mayan Calendar claims argmageddon. Don't plan vacations after this."]
         }]
--- weathermashup/web.py
+++ weathermashup/web.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request
 from lookup import reports_by_location
+
+from datetime import datetime
+import json 
+
 app = Flask(__name__)
+
+def json_types(obj):
+    if isinstance(obj, datetime):
+        return obj.strftime("%s")
+    raise TypeError()
+
+def to_json(obj):
+    return json.dumps(obj, default=json_types, indent=2)
 
 @app.route("/")
 def index(input_warning=None):
@@ -13,8 +25,11 @@ def timeline():
     if not location or len(location) < 3:
         return index(input_warning="Bitte gib einen Ort ein!")
 
+    reports = reports_by_location(location)
+    reports_json = to_json(reports)
     return render_template("timeline.html", 
-            reports=reports_by_location(location))
+            reports=reports,
+            reports_json=reports_json)
 
 
 if __name__ == "__main__":
