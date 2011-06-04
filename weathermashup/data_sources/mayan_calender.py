20097c835416cd61cbd123ba152a97b29dffd2ee
Friedrich Lindenberg <friedrich@pudo.org>
2011-06-04 18:00:22 +0200

    basic logic to support time series deviation/outliers detection

--- weathermashup/data_sources/mayan_calender.py
+++ weathermashup/data_sources/mayan_calender.py
@@ -5,6 +5,7 @@ def armageddon(location):
         return []
     return [{
         "time_from": datetime(2012, 12, 12),
+        "time_from": datetime.max(),
         "temperature_max": 999999999,
         "temperature_min": 999999998,
         "time_to": "end of time", #datetime(2999, 1, 1),
--- weathermashup/web.py
+++ weathermashup/web.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from lookup import reports_by_location
+from logic import reports_time_series
 
 from datetime import datetime
 import json 
@@ -26,11 +27,11 @@ def timeline():
         return index(input_warning="Bitte gib einen Ort ein!")
 
     reports = reports_by_location(location)
-    reports_json = to_json(reports)
+    time_series = reports_time_series(reports)
+    time_series_json = to_json(time_series)
     return render_template("timeline.html", 
             location=location,
-            reports=reports,
-            reports_json=reports_json)
+            time_series_json=time_series_json)
 
 
 if __name__ == "__main__":
