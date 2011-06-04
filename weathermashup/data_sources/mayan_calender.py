d0936602e5339d459d2065bec08312c4d6c617fe
Friedrich Lindenberg <friedrich@pudo.org>
2011-06-04 14:31:41 +0200

    basic templates and support for mayan armageddon

---
+++ weathermashup/data_sources/mayan_calender.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def armageddon(location):
+    return [{
+        "time_from": "2012", #datetime(2012, 1, 1),
+        "time_to": "end of time", #datetime(2999, 1, 1),
+        "warnings": ["Mayan Calendar claims argmageddon. Don't plan vacations after this."]
+        }]
--- weathermashup/web.py
+++ weathermashup/web.py
@@ -1,10 +1,21 @@
-from flask import Flask 
-
+from flask import Flask, render_template, request
+from lookup import reports_by_location
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "hellO"
+def index(input_warning=None):
+    return render_template("index.html",
+            input_warning=input_warning)
+
+@app.route("/timeline")
+def timeline():
+    location = request.args.get('location')
+    if not location or len(location) < 3:
+        return index(input_warning="Bitte gib einen Ort ein!")
+
+    return render_template("timeline.html", 
+            reports=reports_by_location(location))
+
 
 if __name__ == "__main__":
     app.debug = True
